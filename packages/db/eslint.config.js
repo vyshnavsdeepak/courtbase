6d749f5bef22092c7b1b543f4fca4b2ad0a68823
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-08-04 16:22:03 +0530

    eslint ignore kysely types - fix DB error in KyselyAuth

--- packages/db/eslint.config.js
+++ packages/db/eslint.config.js
@@ -3,7 +3,7 @@ import baseConfig, { restrictEnvAccess } from "@court-base/eslint-config/base";
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: ["dist/**"],
+    ignores: ["dist/**", "kysely/types.ts"],
   },
   ...baseConfig,
   ...restrictEnvAccess,
--- packages/db/kysely/types.ts
+++ packages/db/kysely/types.ts
@@ -6,7 +6,7 @@ export type Generated<T> =
     : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
-export interface Account {
+export type Account = {
   userId: string;
   type: string;
   provider: string;
@@ -18,8 +18,8 @@ export interface Account {
   scope: string | null;
   id_token: string | null;
   session_state: string | null;
-}
-export interface Case {
+};
+export type Case = {
   id: Generated<string>;
   crn: string;
   title: string;
@@ -27,42 +27,42 @@ export interface Case {
   created_at: Generated<Timestamp>;
   updatedAt: Timestamp | null;
   organizationId: string;
-}
-export interface Organization {
+};
+export type Organization = {
   id: Generated<string>;
   name: string;
   slug: string;
-}
-export interface OrganizationMembers {
+};
+export type OrganizationMembers = {
   organizationId: string;
   userId: string;
   role: string;
-}
-export interface Post {
+};
+export type Post = {
   id: Generated<string>;
   title: string;
   content: string;
   created_at: Generated<Timestamp>;
   updatedAt: Timestamp | null;
-}
-export interface Session {
+};
+export type Session = {
   sessionToken: string;
   userId: string;
   expires: Timestamp;
-}
-export interface User {
+};
+export type User = {
   id: Generated<string>;
   name: string | null;
   email: string;
   emailVerified: Timestamp | null;
   image: string | null;
-}
-export interface VerificationToken {
+};
+export type VerificationToken = {
   token: string;
   identifier: string;
   expires: Timestamp;
-}
-export interface DB {
+};
+export type DB = {
   Account: Account;
   Case: Case;
   Organization: Organization;
@@ -71,4 +71,4 @@ export interface DB {
   Session: Session;
   User: User;
   VerificationToken: VerificationToken;
-}
+};
