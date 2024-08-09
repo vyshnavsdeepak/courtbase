d88c8b5e049cf5063b24c145af49b21199d90260
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-08-09 14:14:05 +0530

    local db fix
    add local db docker file

--- packages/db/env.ts
+++ packages/db/env.ts
@@ -3,4 +3,5 @@ import { z } from "zod";
 
 export const env = {
   DATABASE_URL: z.string().url().parse(process.env.DATABASE_URL),
+  VERCEL_ENV: z.string().optional().parse(process.env.VERCEL_ENV),
 };
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,9 @@
 import type { Codegen } from "@auth/kysely-adapter";
 import { KyselyAuth } from "@auth/kysely-adapter";
+import { neonConfig } from "@neondatabase/serverless";
 import { createPool } from "@vercel/postgres";
 import { PostgresDialect } from "kysely";
+import { NeonDialect } from "kysely-neon";
 
 import type { DB } from "../kysely/types";
 import { env } from "../env";
@@ -9,9 +11,23 @@ import { env } from "../env";
 export { type DB } from "../kysely/types";
 
 export const kysely = new KyselyAuth<DB, Codegen>({
-  dialect: new PostgresDialect({
-    pool: createPool({
-      connectionString: env.DATABASE_URL,
-    }),
-  }),
+  dialect: (() => {
+    if (!env.VERCEL_ENV) {
+      // Set the WebSocket proxy to work with the local instance
+      neonConfig.wsProxy = (host, port) => `${host}:${port}/v1`;
+      // Disable all authentication and encryption
+      neonConfig.useSecureWebSocket = false;
+      neonConfig.pipelineTLS = false;
+      neonConfig.pipelineConnect = false;
+      return new NeonDialect({
+        connectionString: env.DATABASE_URL,
+      });
+    } else {
+      return new PostgresDialect({
+        pool: createPool({
+          connectionString: env.DATABASE_URL,
+        }),
+      });
+    }
+  })(),
 });
