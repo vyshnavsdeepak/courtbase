12d9e258e456b590ba0932be627f73f6c5e969f2
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-08-23 12:53:38 +0530

    add db:generate to build

--- packages/db/eslint.config.js
+++ packages/db/eslint.config.js
@@ -3,7 +3,12 @@ import baseConfig, { restrictEnvAccess } from "@court-base/eslint-config/base";
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: ["dist/**", "kysely/types.ts", "kysely/enums.ts", "src/models/index.ts"],
+    ignores: [
+      "dist/**",
+      "kysely/types.ts",
+      "kysely/enums.ts",
+      "src/models/index.ts",
+    ],
   },
   ...baseConfig,
   ...restrictEnvAccess,
