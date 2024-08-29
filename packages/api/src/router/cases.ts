2e61892df60d76a3886dd4fcd88ed0beb44877cd
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-08-29 06:17:52 +0530

    add case sort by next hearing date

--- packages/api/src/router/cases.ts
+++ packages/api/src/router/cases.ts
@@ -50,6 +50,7 @@ const casesRouter = {
           "Case.updatedAt as updatedAt",
         ])
         .where("Case.organizationId", "=", orgId)
+        .orderBy("nextHearingDate", "desc")
         .$if(typeof sort?.field !== "undefined", (query) => {
           if (!sort?.field) {
             throw new Error("Invalid sort field or direction");
