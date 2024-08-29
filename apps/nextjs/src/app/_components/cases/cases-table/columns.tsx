ea92cb805e3f7ddfd92a53e048805d196ac532a1
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-08-29 06:02:21 +0530

    add moment.js for last updated at

--- apps/nextjs/src/app/_components/cases/cases-table/columns.tsx
+++ apps/nextjs/src/app/_components/cases/cases-table/columns.tsx
@@ -2,6 +2,7 @@
 
 import type { ColumnDef } from "@tanstack/react-table";
 import type { z } from "zod";
+import moment from "moment";
 
 import { AllCaseResponseSchema } from "@court-base/api/schemas/cases";
 
@@ -16,6 +17,7 @@ const _CaseTableRowsSchema = AllCaseResponseSchema.pick({
   nextHearingDate: true,
   advocateId: true,
   advocateName: true,
+  updatedAt: true,
 });
 
 type CaseTableRows = z.infer<typeof _CaseTableRowsSchema>;
@@ -58,31 +60,8 @@ export const columns: ColumnDef<CaseTableRows>[] = [
       return row.original.advocateName;
     },
   },
-];
-
-export const sampleData: CaseTableRows[] = [
   {
-    crn: "1234567890",
-    courtId: "1",
-    courtName: "Test Court",
-    typeName: "OP",
-    number: "1234",
-    regYear: "2021",
-    title: "Test Case",
-    nextHearingDate: new Date("2021-01-01"),
-    advocateId: "1",
-    advocateName: "Test Advocate",
-  },
-  {
-    crn: "1234567891",
-    courtId: "2",
-    courtName: "Test Court 2",
-    typeName: "OP",
-    number: "1235",
-    regYear: "2022",
-    title: "Test Case 2",
-    nextHearingDate: new Date("2022-01-01"),
-    advocateId: "2",
-    advocateName: "Test Advocate 2",
+    header: "Last Updated",
+    cell: ({ row }) => <div>{moment(row.original.updatedAt).fromNow()}</div>,
   },
 ];
