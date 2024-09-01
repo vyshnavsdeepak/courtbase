7d4c370c58d59299fc468112a2f527d0fe987e19
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-09-01 13:31:44 +0530

    fix sentry next config

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from "url";
 import createJiti from "jiti";
+import { withSentryConfig } from "@sentry/nextjs";
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
 createJiti(fileURLToPath(import.meta.url))("./src/env");
@@ -36,15 +37,9 @@ const config = {
   },
 };
 
-export default config;
-
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  module.exports,
+export default withSentryConfig(
+  config,
+  // Injected content via Sentry wizard below
   {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
