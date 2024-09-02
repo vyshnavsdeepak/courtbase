dbe7fef14d2dac8b9e43085008eac3d8ff753c2e
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-09-02 09:13:32 +0530

    fix loading shimmer for cases table

--- apps/nextjs/src/app/(workspace)/x/[slug]/cases/all/page.tsx
+++ apps/nextjs/src/app/(workspace)/x/[slug]/cases/all/page.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import qs from "qs";
 
 import type { CasesResponseTypeComplete } from "@court-base/api/schemas/cases";
@@ -32,12 +32,32 @@ async function fetchCasesData(
   return { casesData, casesCount };
 }
 
-export default async function CasesPage({
+const CasesMainComponent = ({
+  promise,
+}: {
+  promise: Promise<CasesPageData>;
+}) => {
+  const { casesData, casesCount } = React.use(promise);
+  return (
+    <>
+      {casesCount > 0 ? (
+        <>
+          <DatePickerWithPresets />
+          <CaseTable columns={columns} data={casesData.data} />
+        </>
+      ) : (
+        <EmptyCases />
+      )}
+    </>
+  );
+};
+
+export default function CasesPage({
   searchParams,
 }: {
   searchParams: SearchParams;
 }) {
-  const { casesData, casesCount } = await fetchCasesData(searchParams);
+  const promise = fetchCasesData(searchParams);
   const key = qs.stringify(searchParams);
   return (
     <div className="flex h-full min-h-screen flex-col">
@@ -46,27 +66,29 @@ export default async function CasesPage({
           <SidebarToggle className="lg:hidden" />
         </div>
         <div className="flex flex-1">
-          {casesCount > 0 ? (
-            <div className="mt-2 flex w-full flex-col gap-4">
-              <Suspense
-                key={key}
-                fallback={
-                  <DataTableSkeleton
-                    columnCount={5}
-                    searchableColumnCount={1}
-                    filterableColumnCount={2}
-                    cellWidths={["10rem", "40rem", "12rem", "12rem", "8rem"]}
-                    shrinkZero
-                  />
-                }
-              >
-                <DatePickerWithPresets />
-                <CaseTable columns={columns} data={casesData.data} />
-              </Suspense>
-            </div>
-          ) : (
-            <EmptyCases />
-          )}
+          <div className="mt-2 flex max-w-full flex-col gap-4">
+            <Suspense
+              key={key}
+              fallback={
+                <DataTableSkeleton
+                  columnCount={7}
+                  searchableColumnCount={1}
+                  filterableColumnCount={2}
+                  cellWidths={[
+                    "10rem",
+                    "10rem",
+                    "10rem",
+                    "20rem",
+                    "8rem",
+                    "8rem",
+                    "8rem",
+                  ]}
+                />
+              }
+            >
+              <CasesMainComponent promise={promise} />
+            </Suspense>
+          </div>
         </div>
       </div>
     </div>
