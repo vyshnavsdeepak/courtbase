6a6580df075ec6004831a0cd67cd329d0e8abd75
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-09-11 19:21:26 +0530

    change unstable_httpBatchStreamLink to httpBatchLink

--- apps/nextjs/src/trpc/react.tsx
+++ apps/nextjs/src/trpc/react.tsx
@@ -3,7 +3,7 @@
 import type { QueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import SuperJSON from "superjson";
 
@@ -39,7 +39,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             env.NODE_ENV === "development" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
-        unstable_httpBatchStreamLink({
+        httpBatchLink({
           transformer: SuperJSON,
           url: getBaseUrl() + "/api/trpc",
           headers() {
